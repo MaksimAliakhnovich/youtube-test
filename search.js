dd2441b81263ccd82d74361ede42ed23e46af56d
Maksim <33697594+MaksimAliakhnovich@users.noreply.github.com>
2018-11-25 15:06:23 +0300

    Add files via upload

--- search.js
+++ search.js
@@ -3,7 +3,9 @@ const youtubeLinkWithoutId = 'https://www.youtube.com/watch?v=';
 const videoInYoutubeResponse = 15;
 const resolutions = [1920, 1366, 960, 600, 0];
 const videoOnPageVariants = [4, 3, 2, 1];
-const currentListVideo = [];
+const bufferPagesIndex = 3;
+const minLengthForSwipe = 100;
+const listVideo = [];
 const listVideoViewRate = {};
 let currentAmountVideoOnPage = 1;
 let offsetPage = 0;
@@ -11,6 +13,7 @@ let nextYoutubePageToken = null;
 const body = document.querySelector('body');
 const createDiv = () => document.createElement('div');
 const createSection = () => document.createElement('section');
+const videoContainer = () => document.querySelector('.video-list');
 let section = createSection();
 
 const main = document.createElement('main');
@@ -47,6 +50,13 @@ const defineVideoCount = () => {
   return currentAmountVideoOnPage;
 };
 
+// добавление контейнера для блоков видео
+const createContainer = function createContainerForBlockVideo() {
+  section = createSection();
+  section.classList.add('video-list');
+  main.appendChild(section);
+};
+
 // запрос статистики по конкретному видео
 const getStatisticsVideo = function getStatisticByVideoIdFromYoutube(id) {
   return new Promise((resolve) => {
@@ -75,36 +85,18 @@ const getListStatisticsVideo = function getStatisticAllVideoFromYoutube(list) {
 };
 
 // получение первого листа
-const getListVideo = function getListVideoFromYoutube(resolve) {
+const getFirstYoutubePage = function getListVideoFromYoutube(resolve) {
   const xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function request() {
     if (xhr.readyState === 4 && xhr.status === 200) {
-      JSON.parse(xhr.response).items.forEach(video => currentListVideo.push(video));
-      resolve(JSON.parse(xhr.response).items);
+      JSON.parse(xhr.response).items.forEach(video => listVideo.push(video));
       nextYoutubePageToken = JSON.parse(xhr.response).nextPageToken;
+      resolve(JSON.parse(xhr.response).items);
     }
   };
   xhr.open('GET', `https://www.googleapis.com/youtube/v3/search?key=${apiKey}&type=video&part=snippet&maxResults=${videoInYoutubeResponse}&q=${inputRequest.value}`, true);
   xhr.setRequestHeader('Accept', 'application/json;charset=UTF-8');
   xhr.send();
-};
-
-// добавление контента в блок
-const addContentInBlock = function addAllContentInBlock(video, blockNumber) {
-  const videoThumbnails = document.querySelectorAll('.thumbnails');
-  const videoTitle = document.querySelectorAll('.title');
-  const videoAuthor = document.querySelectorAll('.author span');
-  const videoPublicationDate = document.querySelectorAll('.publication-date span');
-  const videoViewRate = document.querySelectorAll('.view-rate span');
-  const videoDescription = document.querySelectorAll('.description p');
-  videoThumbnails[blockNumber].src = video.snippet.thumbnails.high.url;
-  videoThumbnails[blockNumber].alt = video.snippet.title;
-  videoTitle[blockNumber].innerText = video.snippet.title;
-  videoTitle[blockNumber].href = youtubeLinkWithoutId.concat(video.id.videoId);
-  videoAuthor[blockNumber].innerText = video.snippet.channelTitle;
-  videoPublicationDate[blockNumber].innerText = video.snippet.publishedAt.match(/[0-9]+-[0-9]+-[0-9]+/);
-  videoViewRate[blockNumber].innerText = listVideoViewRate[video.id.videoId];
-  videoDescription[blockNumber].innerText = video.snippet.description;
 };
 
 // создание пустого блока видео
@@ -130,16 +122,27 @@ const createBlockVideo = function createBlockVideoOnDocument() {
   return blockVideo;
 };
 
-// запросы на ютуб
-const getContetntFromYoutube = function getVideoAndStatisticsFromYoutube() {
-  return new Promise(getListVideo).then(getListStatisticsVideo);
+// добавление контента в блок
+const addContentInBlock = function addAllContentInBlock(video, blockNumber) {
+  const videoThumbnails = document.querySelectorAll('.thumbnails');
+  const videoTitle = document.querySelectorAll('.title');
+  const videoAuthor = document.querySelectorAll('.author span');
+  const videoPublicationDate = document.querySelectorAll('.publication-date span');
+  const videoViewRate = document.querySelectorAll('.view-rate span');
+  const videoDescription = document.querySelectorAll('.description p');
+  videoThumbnails[blockNumber].src = video.snippet.thumbnails.high.url;
+  videoThumbnails[blockNumber].alt = video.snippet.title;
+  videoTitle[blockNumber].innerText = video.snippet.title;
+  videoTitle[blockNumber].href = youtubeLinkWithoutId.concat(video.id.videoId);
+  videoAuthor[blockNumber].innerText = video.snippet.channelTitle;
+  videoPublicationDate[blockNumber].innerText = video.snippet.publishedAt.match(/[0-9]+-[0-9]+-[0-9]+/);
+  videoViewRate[blockNumber].innerText = listVideoViewRate[video.id.videoId];
+  videoDescription[blockNumber].innerText = video.snippet.description;
 };
 
-// добавление контейнера для блоков видео
-const createContainer = function createContainerForBlockVideo() {
-  section = createSection();
-  section.classList.add('video-list');
-  main.appendChild(section);
+// запросы на ютуб
+const getContetntFromYoutube = function getVideoAndStatisticsFromYoutube(list) {
+  return new Promise(list).then(getListStatisticsVideo);
 };
 
 // первоначальный вывод результатов поиска в блоки
@@ -147,18 +150,17 @@ const addContentOnPage = function addAllBlocksVideoOnPage() {
   for (let i = 0; i < currentAmountVideoOnPage; i += 1) {
     section.appendChild(createBlockVideo());
   }
-  getContetntFromYoutube().then((promises) => {
+  getContetntFromYoutube(getFirstYoutubePage).then((promises) => {
     for (let i = 0; i < currentAmountVideoOnPage; i += 1) {
-      promises[i].then(() => addContentInBlock(currentListVideo[i], i));
+      promises[i].then(() => addContentInBlock(listVideo[i], i));
     }
   });
 };
 
 // удаление старых блоков видео со страницы
-const clearPage = function deleteAllBlocksVideoFromPage() {
-  const oldVideoList = document.querySelector('.video-list');
-  if (oldVideoList) {
-    oldVideoList.remove();
+const refreshContainer = function refreshVideoContainer() {
+  if (videoContainer()) {
+    videoContainer().remove();
   }
   defineVideoCount();
   createContainer();
@@ -169,8 +171,8 @@ const searchButton = document.querySelector('.search');
 searchButton.addEventListener('click', (e) => {
   e.preventDefault();
   offsetPage = 0;
-  currentListVideo.length = 0;
-  clearPage();
+  listVideo.length = 0;
+  refreshContainer();
   addContentOnPage();
 });
 
@@ -184,65 +186,99 @@ const changeNumberVideoBlock = function changeNumberVideoBlockOnPage() {
   } else {
     for (let i = 0; i < dif; i += 1) {
       section.appendChild(createBlockVideo());
-      addContentInBlock(currentListVideo[tempCur + offsetPage + i], tempCur + i);
+      addContentInBlock(listVideo[tempCur + offsetPage + i], tempCur + i);
     }
   }
 };
 window.addEventListener('resize', changeNumberVideoBlock);
 
+// создание блоков и заполнение контентом при листании страниц
+const fillPrevNextPage = function addBlocksAndContentPrevNextPage() {
+  for (let i = offsetPage; i < offsetPage + currentAmountVideoOnPage; i += 1) {
+    section.appendChild(createBlockVideo());
+    addContentInBlock(listVideo[i], i - offsetPage);
+  }
+};
+
 // следующая страница ютуба
-const nextPageYoutube = function getNextPageYoutube() {
+const addNextYoutubePage = function getAndAddNextYoutubePage(resolve) {
   const xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function reqest() {
     if (xhr.readyState === 4 && xhr.status === 200) {
-      JSON.parse(xhr.response).items.forEach(video => currentListVideo.push(video));
+      JSON.parse(xhr.response).items.forEach(video => listVideo.push(video));
+      nextYoutubePageToken = JSON.parse(xhr.response).nextPageToken;
+      resolve(JSON.parse(xhr.response).items);
     }
   };
   xhr.open('GET', `https://www.googleapis.com/youtube/v3/search?key=${apiKey}&type=video&part=snippet&maxResults=${videoInYoutubeResponse}&pageToken=${nextYoutubePageToken}&q=${inputRequest.value}`, true);
   xhr.setRequestHeader('Accept', 'application/json;charset=UTF-8');
   xhr.send();
 };
-const nextPageYoutubeButton = document.querySelector('.nextYoutube');
-nextPageYoutubeButton.addEventListener('click', nextPageYoutube);
-
-// создание блоков и заполнение контентом при листании страниц
-const fillPrevNextPage = function addBlocksAndContentPrevNextPage() {
-  for (let i = offsetPage; i < offsetPage + currentAmountVideoOnPage; i += 1) {
-    section.appendChild(createBlockVideo());
-    addContentInBlock(currentListVideo[i], i - offsetPage);
-  }
-};
 
 // следующая страница из кэша
 const nextPage = function getNextPage() {
-  clearPage();
+  if (listVideo.length === 0) {
+    return;
+  }
+  const remaringPages = listVideo.length - offsetPage - currentAmountVideoOnPage;
+  if (remaringPages < currentAmountVideoOnPage * bufferPagesIndex) {
+    getContetntFromYoutube(addNextYoutubePage);
+  }
+  refreshContainer();
   offsetPage += currentAmountVideoOnPage;
   fillPrevNextPage();
 };
 
 // предыдущая страница из кэша
 const prevPage = function getPrevPage() {
-  clearPage();
+  if (offsetPage <= 0) {
+    return;
+  }
+  refreshContainer();
   offsetPage -= currentAmountVideoOnPage;
   fillPrevNextPage();
 };
 
-// события по тач свайпу
+// действие при отпускании тача или мыши
 let xDown = null;
-const touchEnd = function touchEndAction(e) {
-  const xUp = e.changedTouches[0].clientX;
+const actionEnd = function mouseOrTouchEndAction(e) {
+  let xUp = null;
+  if (e.type === 'mouseup') {
+    xUp = e.clientX;
+  } else if (e.type === 'touchend') {
+    xUp = e.changedTouches[0].clientX;
+  }
   const xDiff = xDown - xUp;
-  if (Math.abs(xDiff) > 100) {
+  if (Math.abs(xDiff) > minLengthForSwipe) {
     if (xDiff < 0) {
-      prevPage();
+      videoContainer().classList.add('-swipe-right');
+      setTimeout(prevPage, 500);
     } else {
-      nextPage();
+      videoContainer().classList.add('-swipe-left');
+      setTimeout(nextPage, 500);
     }
   }
   xDown = null;
 };
 
+// события по тач свайпу
 main.addEventListener('touchstart', (e) => {
   xDown = e.touches[0].clientX;
 });
-main.addEventListener('touchend', touchEnd);
+main.addEventListener('touchend', actionEnd);
+
+// события по мышь свайпу
+main.addEventListener('mousedown', (e) => {
+  xDown = e.clientX;
+});
+main.addEventListener('mouseup', actionEnd);
+
+// события по нажатию на стрелки
+window.addEventListener('keydown', (e) => {
+  if (e.key === 'ArrowLeft') {
+    prevPage();
+  }
+  if (e.key === 'ArrowRight') {
+    nextPage();
+  }
+});
